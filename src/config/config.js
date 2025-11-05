fd5d2a06924132b9ae4e96e3d3d95b5ab58cc6e6
ankitmishra <meankitmishra10@gmail.com>
2025-11-05 16:06:20 +0530

    change in config

--- src/config/config.js
+++ src/config/config.js
@@ -6,17 +6,17 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports= {
   development: {
-    username: process.env.DATABASE_USERNAME ,
-    password: process.env.DATABASE_PASSWORD ,
+    username: postgres, // change this as per your local setup
+    password: "" ,
     database: "Flights",
-    host: process.env.DATABASE_URL,
+    host: "127.0.0.1",
     dialect: 'postgres'
   },
   test: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+    username: postgres, // change this as per your local setup
+    password: "" ,
     database: "Flights",
-    host: process.env.DATABASE_URL,
+    host: "127.0.0.1",
     dialect: 'postgres'
   },
   production: {
